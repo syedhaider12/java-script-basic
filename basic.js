1d3beddb2486806f8f6a633d08fa3431fa9f3942
haider <haider@.com>
2018-10-04 13:42:43 +0500

    added readme

--- basic.js
+++ basic.js
@@ -1,2 +1,33 @@
 let name ='haider';
 console.log(name);
+//objects
+let person={
+    name:'haider abbas',
+    age:30
+    
+};
+
+console.log(person);
+console.log(person.name);
+//arrays
+let colors =['red','white'];
+console.log(colors);
+console.log(colors[1]);
+//array length
+console.log(colors.length);
+//function
+function output(){
+    console.log("i love my pakistan");
+}
+output();
+//squre number
+function squre (number)
+{
+    return number*number;
+}
+console.log(squre(3));
+//add function
+function add(number,number){
+    return number+number;
+}
+console.log(3+5);
